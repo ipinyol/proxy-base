cdec36911756302352934b9b0e4df097f07dd4c3
ipinyol <ipinyol@gmail.com>
2014-10-15 16:10:26 +0200

    Still building the Rule Engine

--- src/org/mars/proxybase/Rule.java
+++ src/org/mars/proxybase/Rule.java
@@ -1,5 +1,8 @@
 package org.mars.proxybase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rule {
 	private String fire=null;
 	private String port=null;
@@ -17,26 +20,35 @@ public class Rule {
 			System.out.println(rule.paramsToStar("/pepe/{PORT}"));
 			System.out.println(rule.paramsToStar("/pepe/hola"));
 			rule.setFire("/pepe/{PORT}/hola/{CACA}/{ULL}/hola");
-			System.out.println(rule.isFired("/pepe/1111/hola/qqq/ppp/hola"));
-			System.out.println(rule.isFired("/pepe"));
-			System.out.println(rule.isFired("/pepe/hola/1111/qqq/ppp/hola"));
-			System.out.println(rule.isFired(""));
-			System.out.println(rule.isFired("sddsddfsdfsdfsdfsdf"));
-			System.out.println(rule.isFired("/pepe//hola///hola"));
+			System.out.println(rule.isExecuted("/pepe/1111/hola/qqq/ppp/hola"));
+			System.out.println(rule.isExecuted("/pepe"));
+			System.out.println(rule.isExecuted("/pepe/hola/1111/qqq/ppp/hola"));
+			System.out.println(rule.isExecuted(""));
+			System.out.println(rule.isExecuted("sddsddfsdfsdfsdfsdf"));
+			System.out.println(rule.isExecuted("/pepe//hola///hola"));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 	}
 	
-	/*
-	public Appliance applyRule(String input) {
-		if (!isFired(input)) return null;
-		
-	}
-	*/
 	
-	private boolean isFired(String input) {
+	/**
+	 * Return null if the rule does not apply to the given input, an Appliance if it does
+	 * @param input
+	 * @return
+	 */
+	public Appliance applyRule(String input) {
+		if (!isExecuted(input)) return null;
+		Appliance out = new Appliance();
+		Map<String,String> map = new HashMap<String, String>();
+		
+		return out;
+	}
+	
+	
+	private boolean isExecuted(String input) {
 		if (fire==null) return false;
 		if (fire.length()==0) return true;
 		if (input==null) return false;
@@ -46,7 +58,7 @@ public class Rule {
 		return input.matches(regExp);
 	}
 	
-	public String paramsToStar(String in) throws Exception {
+	private String paramsToStar(String in) throws Exception {
 		String out="";
 		int pointer=0;
 		in = in.replace("*", ".*");
